114f7136f253e37e23382fb0e463649e2e54a4ac
Vance Ng <vancejng@gmail.com>
2015-04-28 22:54:00 +0000

    finished assignment. forgot to commit other features

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 LighthouseForum::Application.routes.draw do
+
+  resources :posts
+  root to: 'posts#index'
+  
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +59,4 @@ LighthouseForum::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
